2282024ec290b0cc0ed437f4338d3504e47339ff
Campos Reis <luan@debian.campos>
2021-02-10 17:04:16 -0300

    finalizando CRUD de usuários

--- src/server.js
+++ src/server.js
@@ -5,6 +5,19 @@ const routes = require("./routes");
 const PORT = 3333;
 const app = express();
 
+// Middlewares
+app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
+
+// Not Found
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
